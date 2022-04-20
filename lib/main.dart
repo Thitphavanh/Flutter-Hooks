ffd67a036a32c28378ddcf8cd15c3c30bcdaa373
hery <thitphavanh.psv@gmail.com>
2022-04-20 22:02:15 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -59,9 +60,62 @@ class HomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final opacity = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+    final size = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      lowerBound: 0.0,
+      upperBound: 1.0,
+    );
+    final controller = useScrollController();
+    useEffect(() {
+      controller.addListener(() {
+        final newOpacity = max(imageHeight - controller.offset, 0.0);
+        final normalized = newOpacity.normalized(0.0, imageHeight).toDouble();
+        opacity.value = normalized;
+        size.value = normalized;
+      });
+      return null;
+    }, [controller]);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          SizeTransition(
+            sizeFactor: size,
+            axis: Axis.vertical,
+            axisAlignment: -1.0,
+            child: FadeTransition(
+              opacity: opacity,
+              child: Image.network(
+                url,
+                height: imageHeight,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              controller: controller,
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    'Person ${index + 1}',
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
