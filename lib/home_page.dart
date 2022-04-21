52e6dc8eb72c13e60cb9eb2a710f42930a973317
hery <thitphavanh.psv@gmail.com>
2022-04-21 07:50:34 +0700

    update

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,44 +1,57 @@
-import 'dart:async';
 import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 const url = 'https://bit.ly/3x7J5Qt';
+
+enum Action {
+  rotateLeft,
+  rotateRight,
+  moreVisible,
+  lessVisible,
+}
+
+@immutable
+class State {
+  final double rotationDeg;
+  final double alpha;
+
+  const State({
+    required this.rotationDeg,
+    required this.alpha,
+  });
+  const State.zero()
+      : rotationDeg = 0.0,
+        alpha = 1.0;
+
+  State rotateRight() => State(
+        alpha: alpha,
+        rotationDeg: rotationDeg + 10.0,
+      );
+  State rotateLeft() => State(
+        alpha: alpha,
+        rotationDeg: rotationDeg - 10.0,
+      );
+
+  State increaseAlpha() => State(
+        alpha: min(alpha + 0.1, 1.0),
+        rotationDeg: rotationDeg,
+      );
+  State decreaseAlpha() => State(
+        alpha: max(alpha - 0.1, 0.0),
+        rotationDeg: rotationDeg,
+      );
+}
 
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    late final StreamController<double> controller;
-    controller = useStreamController<double>(onListen: (() {
-      controller.sink.add(0.0);
-    }));
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
-      ),
-      body: StreamBuilder<double>(
-        stream: controller.stream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const CircularProgressIndicator();
-          } else {
-            final rotation = snapshot.data ?? 0.0;
-            return GestureDetector(
-              onTap: () {
-                controller.sink.add(rotation + 10.0);
-              },
-              child: RotationTransition(
-                turns: AlwaysStoppedAnimation(rotation / 360.0),
-                child: Center(
-                  child: Image.network(url),
-                ),
-              ),
-            );
-          }
-        },
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,15 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([
-    E? Function(T?)? transform,
-  ]) =>
-      map(transform ?? (e) => e).where((e) => e != null).cast();
-}
-
-void testIt() {
-  final values = [1, 2, null, 3];
-  final nonNullValues = values.compactMap((e) {
-    if (e != null && e > 10) {
-      return e;
-    } else {
-      return null;
-    }
-  });
-}
-
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
-    );
-  }
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    ),
+  );
 }
