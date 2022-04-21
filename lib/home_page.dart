480e36107bc19c40c2c3a87110fa7bf8652e4ecf
hery <thitphavanh.psv@gmail.com>
2022-04-21 08:12:56 +0700

    step-7

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -44,15 +44,139 @@ class State {
       );
 }
 
+State reducer(State oldState, Action? action) {
+  switch (action) {
+    case Action.rotateLeft:
+      return oldState.rotateLeft();
+
+    case Action.rotateRight:
+      return oldState.rotateRight();
+
+    case Action.moreVisible:
+      return oldState.increaseAlpha();
+
+    case Action.lessVisible:
+      return oldState.decreaseAlpha();
+
+    case null:
+      return oldState;
+  }
+}
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = useReducer<State, Action?>(
+      reducer,
+      initialState: const State.zero(),
+      initialAction: null,
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              RotateLeftButton(store: store),
+              RotateRightButton(store: store),
+              DecreaseAlphaButton(store: store),
+              IncreaseAlphaButton(store: store),
+            ],
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          Opacity(
+            opacity: store.state.alpha,
+            child: RotationTransition(
+              turns: AlwaysStoppedAnimation(
+                store.state.rotationDeg / 360.0,
+              ),
+              child: Image.network(url),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IncreaseAlphaButton extends StatelessWidget {
+  const IncreaseAlphaButton({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.moreVisible);
+      },
+      child: const Text('+ Alpha'),
+    );
+  }
+}
+
+class DecreaseAlphaButton extends StatelessWidget {
+  const DecreaseAlphaButton({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.lessVisible);
+      },
+      child: const Text('- Alpha'),
+    );
+  }
+}
+
+class RotateRightButton extends StatelessWidget {
+  const RotateRightButton({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.rotateRight);
+      },
+      child: const Text('Rotate Right'),
+    );
+  }
+}
+
+class RotateLeftButton extends StatelessWidget {
+  const RotateLeftButton({
+    Key? key,
+    required this.store,
+  }) : super(key: key);
+
+  final Store<State, Action?> store;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        store.dispatch(Action.rotateLeft);
+      },
+      child: const Text('Rotate Left'),
     );
   }
 }
